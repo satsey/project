79be652c41892dbce052b36cfe209c8e2ca1a698
satsey <54670828+satsey@users.noreply.github.com>
2021-06-13 14:02:50 +0800

    update

--- Assets/Scripts/General/GameManager.cs
+++ Assets/Scripts/General/GameManager.cs
@@ -1,23 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Pathfinding;
+
 
 
 
 public class GameManager : MonoBehaviour
 {
 
-    private GameObject tileMap;
-
-    public GameObject Box;
+   
     // At Start, Spawn the map
     void Start()
     {
        
-        // recalculate graph
-        //GridGraph graphToScan = AstarPath.active.data.gridGraph;
-        //AstarPath.active.Scan(graphToScan);
+       
     }
 
     // Update is called once per frame
--- Assets/Scripts/General/TileMap.cs
+++ Assets/Scripts/General/TileMap.cs
@@ -1,13 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class TileMap : MonoBehaviour
 {
-    public void Init()
+    public GameObject Tile;
+
+    public int mapsize = 100;
+
+    private int halfmapsize = 100 / 2;
+
+    void Start()
     {
-        grid = new Tile[10, 10];
+        grid = new GameObject[mapsize, mapsize];
+
+        for (int i = 0; i < mapsize; i++)
+        {
+            for (int j = 0; j < mapsize; j++)
+            {
+                Instantiate(Tile, new Vector3(-halfmapsize + i * 2, 0, -halfmapsize + j * 2), Quaternion.identity);
+            }
+        }
+
+        // recalculate graph
+        GridGraph graphToScan = AstarPath.active.data.gridGraph;
+        AstarPath.active.Scan(graphToScan);
     }
 
-    private Tile[,] grid;
+    private GameObject[,] grid;
 }
